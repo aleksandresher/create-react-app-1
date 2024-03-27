10bb2fa655e133651ee7ad7d2c673d88ef569f38
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-03-27 19:54:34 +0400

    change see more button

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom";
 
 export default function BlogPostCard({ id, title, description, pubdate }) {
   return (
-    <div className="flex flex-col items-center gap-3 p-6 rounded-md bg-[#fff]">
+    <div className="flex flex-col items-center justify-between gap-3 p-6 rounded-md bg-[#fff]">
       <h1 className="font-bold text-center">{title}</h1>
       <p className="text-center">{description}</p>
       <img src="./assets/blog.png" alt={title} className="w-[300px]" />
       <time>{pubdate}</time>
-      <NavLink className="font-medium" to={`/blog/${id}`}>
-        See more
+      <NavLink className="font-medium self-bottom" to={`/blog/${id}`}>
+        <button>Read more</button>
       </NavLink>
     </div>
   );
