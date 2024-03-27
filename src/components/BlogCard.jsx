3818badf5c47e46936bc4aeff8a08f4d222712bb
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-03-27 23:41:24 +0400

    blog page done

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 export default function BlogPostCard({ id, title, description, pubdate }) {
   return (
     <div className="flex flex-col items-center justify-between gap-3 p-6 rounded-md bg-[#fff]">
-      <h1 className="font-bold text-center">{title}</h1>
+      <h1 className="font-bold text-center h-[40px]">{title}</h1>
       <p className="text-center">{description}</p>
       <img src="./assets/blog.png" alt={title} className="w-[300px]" />
-      <time>{pubdate}</time>
+      <time className="underline underline-offset-2 ">{pubdate}</time>
       <NavLink className="font-medium self-bottom" to={`/blog/${id}`}>
-        <button>Read more</button>
+        <button className="hover:text-green-600">Read more</button>
       </NavLink>
     </div>
   );
