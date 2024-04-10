8ea1c015a4294e288c8d156b782957859028cf60
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-04-10 12:37:56 +0400

    add filter and sort to product list page

--- next-app2/src/components/ProductsList.jsx
+++ next-app2/src/components/ProductsList.jsx
@@ -1,19 +1,55 @@
+"use client";
+import { useState, useEffect } from "react";
 import Card from "./Card";
+import Search from "./Search";
 
 export default function ProductList({ products }) {
-  console.log(products);
+  const [productList, setProductList] = useState(products);
+  const [originalData, setOriginalData] = useState(products);
+  const [search, setSearch] = useState("");
+  console.log(search);
+  const [sortedDescending, setSortedDescending] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const filteredProducts = originalData.filter((item) =>
+        item.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setProductList(filteredProducts);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [search, originalData]);
+
+  const sortProduct = () => {
+    const sortedProducts = [...productList].sort((a, b) =>
+      sortedDescending ? b.price - a.price : a.price - b.price
+    );
+    setProductList(sortedProducts);
+    setSortedDescending(!sortedDescending);
+  };
   return (
-    <section className="w-11/12 grid grid-cols-4 gap-7">
-      {products?.map((item) => {
-        return (
-          <Card
-            key={item.id}
-            title={item.title}
-            image={item.images[0]}
-            price={item.price}
-          />
-        );
-      })}
+    <section className="w-full flex flex-col items-center">
+      <Search
+        search={search}
+        setSearch={setSearch}
+        sort={sortProduct}
+        sorted={sortedDescending}
+      />
+      <div className="w-11/12 grid grid-cols-4 gap-7">
+        {productList?.map((item) => {
+          return (
+            <Card
+              key={item.id}
+              title={item.title}
+              image={item.images[0]}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 }
