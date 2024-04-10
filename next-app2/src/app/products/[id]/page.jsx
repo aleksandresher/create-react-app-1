3da184ed211ea3c61fa9c90c4a6e36317d668087
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-04-10 14:04:01 +0400

    add single product dynamic page

---
+++ next-app2/src/app/products/[id]/page.jsx
@@ -0,0 +1,22 @@
+import SingleProduct from "@/components/SIngleProduct";
+
+export async function generateStaticParams() {
+  const res = await fetch(`https://dummyjson.com/products`);
+  const data = res.json();
+
+  if (!data || !data.products) {
+    return [];
+  }
+  return data.products.map((product) => ({
+    id: product.id,
+  }));
+}
+
+export default function SingleProductPage({ params }) {
+  const { id } = params;
+  return (
+    <section>
+      <SingleProduct id={id} />
+    </section>
+  );
+}
--- next-app2/src/components/Card.jsx
+++ next-app2/src/components/Card.jsx
@@ -1,31 +1,34 @@
+"use client";
+import Link from "next/link";
 import Image from "next/image";
-export default function Card({ title, image, price }) {
+export default function Card({ title, image, price, id }) {
   return (
-    <div className="grid grid-cols-1 items-center shadow-lg py-5 gap-2 ">
-      <span className="flex justify-center">
-        {" "}
-        <Image
-          src={image}
-          alt={title}
-          width={300}
-          height={300}
-          className="w-[250px] h-[250px] rounded-lg"
-        />
-      </span>
+    <Link href={`/products/${id}`}>
+      <div className="grid grid-cols-1 items-center shadow-lg py-5 gap-2 ">
+        <span className="flex justify-center">
+          <Image
+            src={image}
+            alt={title}
+            width={300}
+            height={300}
+            className="w-[250px] h-[250px] rounded-lg"
+          />
+        </span>
 
-      <span className="flex flex-col items-center">
-        <h1>{title}</h1>
-        <div className="flex gap-2 items-center h-[40px] mt-2">
-          <p>Price:</p>
-          <p className="text-green-600">{price}$</p>
+        <span className="flex flex-col items-center">
+          <h1>{title}</h1>
+          <div className="flex gap-2 items-center h-[40px] mt-2">
+            <p>Price:</p>
+            <p className="text-green-600">{price}$</p>
+          </div>
+        </span>
+
+        <div className="flex justify-center">
+          <button className="bg-[#faedcd] p-3 rounded-md w-1/2">
+            Add to cart
+          </button>
         </div>
-      </span>
-
-      <div className="flex justify-center">
-        <button className="bg-[#faedcd] p-3 rounded-md w-1/2">
-          Add to cart
-        </button>
       </div>
-    </div>
+    </Link>
   );
 }
