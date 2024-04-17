fa097e45bf6e96f7fa25555e87312d5839388804
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-04-17 17:40:51 +0400

    test

--- next-app2/src/middleware.js
+++ next-app2/src/middleware.js
@@ -1,16 +1,16 @@
-import { redirect } from "next/dist/server/api-utils";
-import { NextResponse } from "next/server";
-import { useRouter } from "next/router";
-const URL = process.env.BASE_URL;
-export function middleware(request) {
-  let cookie = request.cookies.get("auth");
-  console.log(cookie);
-  const url = new URL(request.url);
+// import { redirect } from "next/dist/server/api-utils";
+// import { NextResponse } from "next/server";
+// import { useRouter } from "next/router";
+// const URL = process.env.BASE_URL;
+// export function middleware(request) {
+//   let cookie = request.cookies.get("auth");
+//   console.log(cookie);
+//   const url = new URL(request.url);
 
-  //   const url = request.nextUrl.clone();
-  //   url.pathname = "/login";
+//   //   const url = request.nextUrl.clone();
+//   //   url.pathname = "/login";
 
-  if (!cookie && url.pathname !== `${URL}/login`) {
-    return NextResponse.redirect(new URL(`${URL}/login`, request.url));
-  }
-}
+//   if (!cookie && url.pathname !== `${URL}/login`) {
+//     return NextResponse.redirect(new URL(`${URL}/login`, request.url));
+//   }
+// }
