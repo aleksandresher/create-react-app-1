ccf07d877a8ef5cfe864f4ffba8f6025ffb41c21
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-04-17 17:30:18 +0400

    change urls in middleware

--- next-app2/src/middleware.js
+++ next-app2/src/middleware.js
@@ -1,7 +1,7 @@
 import { redirect } from "next/dist/server/api-utils";
 import { NextResponse } from "next/server";
 import { useRouter } from "next/router";
-
+const URL = process.env.BASE_URL;
 export function middleware(request) {
   let cookie = request.cookies.get("auth");
   console.log(cookie);
@@ -10,7 +10,7 @@ export function middleware(request) {
   //   const url = request.nextUrl.clone();
   //   url.pathname = "/login";
 
-  if (!cookie && url.pathname !== "/login") {
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (!cookie && url.pathname !== `${URL}/login`) {
+    return NextResponse.redirect(new URL(`${URL}/login`, request.url));
   }
 }
